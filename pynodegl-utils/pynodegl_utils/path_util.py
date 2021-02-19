54f047f1d03951c844516e76f29719831cc65956
Jeff Moguillansky <jmoguillansky@gopro.com>
2021-02-19 10:34:20 -0800

    pynodegl-utils: add path utility functions
    Produce a normalized path that works across all platforms.

---
+++ pynodegl-utils/pynodegl_utils/path_util.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#
+# Copyright 2020 GoPro Inc.
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+import platform
+import os.path as op
+
+def path_norm(p):
+    if platform.system() == 'Windows':
+        # This is a trick to produce a path that works in both WSL and Windows
+        p = op.relpath(p)
+        p = p.replace('\\','/')
+    return p
+
+def path_join(path, *paths):
+    return path_norm(op.join(path, *paths))
